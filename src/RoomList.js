e47135caef4f5ab6dc8f7d16364c5bff4937b0a3
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-14 15:14:09 -0400

    Add a 'add room' button

--- src/RoomList.js
+++ src/RoomList.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
 import RoomLink from './RoomLink'
+import RoomForm from './RoomForm'
+
 import base from './base'
 
 class RoomList extends Component {
@@ -9,7 +11,7 @@ class RoomList extends Component {
         rooms: {},
     }
 
-    componentDidMount(){
+    componentDidMount() {
         base.syncState(
             'rooms',
             {
@@ -19,41 +21,51 @@ class RoomList extends Component {
         )
     }
     addRoom = (room) => {
-        const rooms = {...this.state.rooms}
+        const rooms = { ...this.state.rooms }
         rooms[room.name] = room
-        this.setState({rooms})
+        this.setState({ rooms })
     }
-    render(){
-    return (
-        <nav
-            className={`RoomList ${css(styles.nav)}`}
-        >
-            <h2 className={css(styles.h2)}>Rooms</h2>
-            <ul className={css(styles.list)}>
-            {
-              Object.keys(this.state.rooms).map(roomName => (
-                <RoomLink
-                  key={roomName}
-                  room={this.state.rooms[roomName]}
-                  loadRoom={this.props.loadRoom}
-                />
-              ))
-            }
-            </ul>
+    render() {
+        return (
 
-            <form>
+            <nav
+                className={`RoomList ${css(styles.nav)}`}
+            >
+                <div className={css(styles.heading)}>
+                    <h2 className={css(styles.h2)}>Rooms</h2>
+                    <button
+                        className={css(styles.button)}
+                        onClick={this.showRoomForm}
+                    >
+                        <i className="fas fa-plus-circle" title="Add room"></i>
+                    </button>
+                </div>
+
+                <ul className={css(styles.list)}>
+                    {
+                        Object.keys(this.state.rooms).map(roomName => (
+                            <RoomLink
+                                key={roomName}
+                                room={this.state.rooms[roomName]}
+                                loadRoom={this.props.loadRoom}
+                            />
+                        ))
+                    }
+                </ul>
+
+                {/* <form>
                         <input 
                         type="text" 
                         name="roomName" 
                         className={css(styles.input)}
                         placeholder="Enter room name..."/>
                 {/* <input type="submit" value="Submit" /> */}
-            </form>
-            <button type="submit" className={css(styles.add)}>add room</button>
+                {/* </form>
+            <button type="submit" className={css(styles.add)}>add room</button> */}
 
-        </nav>
-    )
-}
+            </nav>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -72,67 +84,70 @@ const styles = StyleSheet.create({
         paddingBottom: "1rem",
     },
 
-    item: {
-        marginBottom: '0.5rem',
-    },
+    heading: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      },
 
-    add: {
-        display: 'block',
-        margin: '0 auto',
-        padding: '.5rem 1rem',
-        fontSize: '1rem',
-        borderRadius: '1rem',
-        backgroundColor: '#316d3f',
-        color: 'whitesmoke',
-        width: '10rem',
-        font: "sans-serif",
-
-        '::before': {
-            content: '" "',
-        },
-
-        ':hover': {
-            backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        }
-    },
-
-    input: {
-        display: 'block',
-        margin: '0 auto',
-        flex: 1,
-        width: '10rem',
-        fontSize: '1rem',
+      button: {
         border: 0,
-        marginBottom: ".5rem",
-        padding: ".5rem .15rem",
-        borderRadius: '1rem',
-        backgroundColor: "rgba(255, 255, 255, 0.2)",
-        color: "whitesmoke",
-        font: "sans-serif",
-
-        ':focus': {
-          outline: 0,
-        },
-        '::placeholder':{
-            color: "whitesmoke",
-            fontSize: "1rem",
-            font: "sans-serif",
+        backgroundColor: 'transparent',
+        outline: 0,
+        padding: 0,
+        fontSize: '1rem',
+        color: 'rgba(255,255,255, 0.4)',
+        cursor: 'pointer',
+        transition: 'color 0.25s ease-out',
+    
+        ':hover': {
+          color: 'white',
         }
       },
 
-    link: {
-        display: 'block',
-        color: 'whitesmoke',
-        textDecoration: 'none',
+    // add: {
+    //     display: 'block',
+    //     margin: '0 auto',
+    //     padding: '.5rem 1rem',
+    //     fontSize: '1rem',
+    //     borderRadius: '1rem',
+    //     backgroundColor: '#316d3f',
+    //     color: 'whitesmoke',
+    //     width: '10rem',
+    //     font: "sans-serif",
 
-        '::before': {
-            content: '"# "',
-        },
+    //     '::before': {
+    //         content: '" "',
+    //     },
 
-        ':hover': {
-            backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        }
-    },
+    //     ':hover': {
+    //         backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    //     }
+    // },
+
+    // input: {
+    //     display: 'block',
+    //     margin: '0 auto',
+    //     flex: 1,
+    //     width: '10rem',
+    //     fontSize: '1rem',
+    //     border: 0,
+    //     marginBottom: ".5rem",
+    //     padding: ".5rem .15rem",
+    //     borderRadius: '1rem',
+    //     backgroundColor: "rgba(255, 255, 255, 0.2)",
+    //     color: "whitesmoke",
+    //     font: "sans-serif",
+
+    //     ':focus': {
+    //         outline: 0,
+    //     },
+    //     '::placeholder': {
+    //         color: "whitesmoke",
+    //         fontSize: "1rem",
+    //         font: "sans-serif",
+    //     }
+    // },
 })
 
 export default RoomList
