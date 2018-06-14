6639961d22bc1b116136d204d81c757c6d94f89f
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-14 00:04:32 -0400

    Add a create room button and text field

--- src/RoomList.js
+++ src/RoomList.js
@@ -5,53 +5,108 @@ import { StyleSheet, css } from 'aphrodite'
 const RoomList = () => {
     return (
         <nav
-          className={`RoomList ${css(styles.nav)}`}
+            className={`RoomList ${css(styles.nav)}`}
         >
-          <h2 className={css(styles.h2)}>Rooms</h2>
-          <ul className={css(styles.list)}>
-            <li className={css(styles.item)}>
-              <a href="#" className={css(styles.link)}>general</a>
-            </li>
-            <li className={css(styles.item)}>
-              <a href="#" className={css(styles.link)}>random</a>
-            </li>
-          </ul>
+            <h2 className={css(styles.h2)}>Rooms</h2>
+            <ul className={css(styles.list)}>
+                <li className={css(styles.item)}>
+                    <a href="#" className={css(styles.link)}>general</a>
+                </li>
+                <li className={css(styles.item)}>
+                    <a href="#" className={css(styles.link)}>random</a>
+                </li>
+            </ul>
+            <form>
+                        <input 
+                        type="text" 
+                        name="roomName" 
+                        className={css(styles.input)}
+                        placeholder="Enter room name..."/>
+                {/* <input type="submit" value="Submit" /> */}
+            </form>
+            <button type="submit" className={css(styles.add)}>add room</button>
+
         </nav>
-      )
+    )
 }
 
 const styles = StyleSheet.create({
     nav: {
         padding: '0 1rem',
-      },
-    
-      h2: {
+    },
+
+    h2: {
         fontSize: '1rem',
-      },
-    
-      list: {
+    },
+
+    list: {
         listStyle: 'none',
         marginLeft: 0,
         paddingLeft: 0,
-      },
-    
-      item: {
+        paddingBottom: "1rem",
+    },
+
+    item: {
         marginBottom: '0.5rem',
+    },
+
+    add: {
+        display: 'block',
+        margin: '0 auto',
+        padding: '.5rem 1rem',
+        fontSize: '1rem',
+        borderRadius: '1rem',
+        backgroundColor: '#316d3f',
+        color: 'whitesmoke',
+        width: '10rem',
+        font: "sans-serif",
+
+        '::before': {
+            content: '" "',
+        },
+
+        ':hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        }
+    },
+
+    input: {
+        display: 'block',
+        margin: '0 auto',
+        flex: 1,
+        width: '10rem',
+        fontSize: '1rem',
+        border: 0,
+        marginBottom: ".5rem",
+        padding: ".5rem .15rem",
+        borderRadius: '1rem',
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        color: "whitesmoke",
+        font: "sans-serif",
+
+        ':focus': {
+          outline: 0,
+        },
+        '::placeholder':{
+            color: "whitesmoke",
+            fontSize: "1rem",
+            font: "sans-serif",
+        }
       },
-    
-      link: {
+
+    link: {
         display: 'block',
         color: 'whitesmoke',
         textDecoration: 'none',
-    
+
         '::before': {
-          content: '"# "',
+            content: '"# "',
         },
-    
+
         ':hover': {
-          backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
         }
-      },
+    },
 })
 
 export default RoomList
