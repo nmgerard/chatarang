ed3e00f63447eac8e030bcc0e871d11932ef197f
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-20 13:58:34 -0400

    remove rooms from roomLists state

--- src/Main.js
+++ src/Main.js
@@ -5,14 +5,14 @@ import Chat from './Chat'
 import base from './base'
 
 class Main extends Component {
-    state = {
-        room: {},
-        rooms: {},
-    }
- 
+  state = {
+    room: {},
+    rooms: {},
+  }
 
-    componentDidMount() {
-        const { roomName } = this.props.match.params
+
+  componentDidMount() {
+    const { roomName } = this.props.match.params
 
     base.syncState(
       'rooms',
@@ -22,64 +22,73 @@ class Main extends Component {
         then: () => this.loadRoom(roomName),
       }
     )
-      }
-    
-      componentDidUpdate(prevProps) {
-        if (prevProps.match.params.roomName !== this.props.match.params.roomName) {
-          this.loadRoom(this.props.match.params.roomName)
-        }
-      }
+  }
 
-      loadRoom = (roomName) => {
-        if (roomName === 'new') return null
-    
-        const room = this.state.rooms[roomName]
-    
-        if (room) {
-          this.setState({ room })
-        } else {
-          this.loadValidRoom()
-        }
-      }
-    
-      loadValidRoom = () => {
-        const realRoomName = Object.keys(this.state.rooms).find(
-            roomName => this.state.rooms[roomName]
-        )
-        this.props.history.push(`/rooms/${realRoomName}`)
-      }
-
-      removeRoom = (room) => {
-        const rooms = {...this.state.rooms}
-        rooms[room.name] = null
-    
-        this.setState(
-          { rooms },
-          this.loadValidRoom
-        )
-      }
-
-    render() {
-        return (
-            <div className="Main" style={styles}>
-                <Sidebar 
-                user={this.props.user} 
-                users={this.props.users}
-                signOut={this.props.signOut} />
-                <Chat 
-                user={this.props.user} 
-                room={this.state.room}
-                removeRoom={this.removeRoom}
-                />
-            </div>
-        )
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.roomName !== this.props.match.params.roomName) {
+      this.loadRoom(this.props.match.params.roomName)
     }
+  }
+
+  loadRoom = (roomName) => {
+    if (roomName === 'new') return null
+
+    const room = this.state.rooms[roomName]
+
+    if (room) {
+      this.setState({ room })
+    } else {
+      this.loadValidRoom()
+    }
+  }
+
+  loadValidRoom = () => {
+    const realRoomName = Object.keys(this.state.rooms).find(
+      roomName => this.state.rooms[roomName]
+    )
+    this.props.history.push(`/rooms/${realRoomName}`)
+  }
+
+  addRoom = (room) => {
+    const rooms = { ...this.state.rooms }
+    rooms[room.name] = room
+    this.setState({ rooms })
+  }
+
+  removeRoom = (room) => {
+    const rooms = { ...this.state.rooms }
+    rooms[room.name] = null
+
+    this.setState(
+      { rooms },
+      this.loadValidRoom
+    )
+  }
+
+  render() {
+    return (
+      <div className="Main" style={styles}>
+        <Sidebar
+          user={this.props.user}
+          users={this.props.users}
+          signOut={this.props.signOut}
+          rooms={this.state.rooms}
+          addRoom={this.addRoom}
+           />
+        <Chat
+          user={this.props.user}
+          room={this.state.room}
+          removeRoom={this.removeRoom}
+        />
+      </div>
+    )
+  }
 }
 
 const styles = {
-    display: "flex",
-    alignItems: "stretch",
-    height: "100vh",
+  display: "flex",
+  alignItems: "stretch",
+  height: "100vh",
 }
 
 export default Main
